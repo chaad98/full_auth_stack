bf30c80dac164ee641649ab3852cdcfc126a1087
chaad98 <faritzchad@yahoo.com>
2025-05-25 07:10:07 +0800

    Implemented sign up server action with form validation

--- apps/frontend/src/app/auth/signup/signUpForm.tsx
+++ apps/frontend/src/app/auth/signup/signUpForm.tsx
@@ -1,17 +1,27 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import SubmitButton from "@/components/ui/submitButton";
-import React from "react";
+import { signUp } from "@/lib/auth";
+import { error } from "console";
+import React, { useActionState } from "react";
 
 const SignUpForm = () => {
+  const [state, action] = useActionState(signUp, undefined);
   return (
-    <form>
+    <form action={action}>
       <div className="flex flex-col gap-2">
+        {state?.message && (
+          <p className="text-sm text-red-500">{state.message}</p>
+        )}
         <div>
           <Label htmlFor="name">Name</Label>
           <Input id="name" name="name" placeholder="Kael Invoker"></Input>
         </div>
+        {state?.error?.name && (
+          <p className="text-sm text-red-500">{state.error.name}</p>
+        )}
 
         <div>
           <Label htmlFor="email">Email</Label>
@@ -21,11 +31,23 @@ const SignUpForm = () => {
             placeholder="invoker@dota2.com"
           ></Input>
         </div>
+        {state?.error?.email && (
+          <p className="text-sm text-red-500">{state.error.name}</p>
+        )}
 
         <div>
           <Label htmlFor="password">Password</Label>
           <Input id="password" name="password" type="password"></Input>
         </div>
+        {state?.error?.password && (
+          <div className="text-sm text-red-500">
+            <ul>
+              {state.error.password.map((e) => (
+                <li key={e}>{e}</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <SubmitButton>Sign Up</SubmitButton>
       </div>
     </form>
---
+++ apps/frontend/src/lib/type.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export type FormState =
+  | {
+      error?: {
+        name?: string[];
+        email?: string[];
+        password?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
+
+export const SignUpFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters long" })
+    .trim(),
+  email: z.string().email({ message: "Please enter a valid email" }).trim(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character",
+    })
+    .trim(),
+});
