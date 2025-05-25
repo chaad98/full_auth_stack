221f8b974992a4e565cffaf0068d2355cf29797d
chaad98 <faritzchad@yahoo.com>
2025-05-25 20:18:25 +0800

    Implemented sign in server action with form validation

--- apps/frontend/src/lib/auth.ts
+++ apps/frontend/src/lib/auth.ts
@@ -2,7 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { BACKEND_URL } from "./constants";
-import { FormState, SignUpFormSchema } from "./type";
+import { FormState, SignInFormSchema, SignUpFormSchema } from "./type";
 
 export async function signUp(
   state: FormState,
@@ -36,6 +36,41 @@ export async function signUp(
         response.status === 409
           ? "The user is already existed!"
           : response.statusText,
+    };
+  }
+}
+
+export async function signIn(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {
+  const validationFields = SignInFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validationFields.success) {
+    return {
+      error: validationFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const response = await fetch(`${BACKEND_URL}/auth/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(validationFields.data),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    // TODO: Create the Session for authenticated user
+    console.log({ result });
+  } else {
+    return {
+      message:
+        response.status === 401 ? "Invalid credentials!" : response.statusText,
     };
   }
 }
