118428f49bc58ebd593fc1803b68ccd850201898
chaad98 <faritzchad@yahoo.com>
2025-06-18 23:49:56 +0800

    chore: include `accessToken` in Session type

--- apps/frontend/src/lib/auth.ts
+++ apps/frontend/src/lib/auth.ts
@@ -72,6 +72,7 @@ export async function signIn(
         id: result.id,
         name: result.name,
       },
+      accessToken: result.accessToken,
     });
 
     redirect("/"); // This endpoint refers as dashboard or home page of our website route
